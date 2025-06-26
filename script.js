d6d13b5f926a81c6cac7627733d71fff514d20e0
AyushKhaitan1 <ayushkhaitan2004@gmail.com>
2025-06-26 23:37:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".Home-text-im",{
+    strings: ["Frontend Developer", "Web Designer", " Problem Solver","Lifelong learner"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true, 
+});
